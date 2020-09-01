bc44bd916144bfb7e2e7706ac56fcf7217a8b7b2
Jillian Kavanaugh <jilliankavanaugh@Jillians-MacBook-Pro.local>
2020-09-01 14:15:23 -0500

    implemented expenses

---
+++ Expenses/Expense.swift
@@ -0,0 +1,15 @@
+//
+//  Expense.swift
+//  Expenses
+//
+//  Created by Jillian Kavanaugh on 8/31/20.
+//  Copyright © 2020 JILL KAVANAUGH. All rights reserved.
+//
+
+import Foundation
+
+struct Expense {
+    let title: String
+    let amount: Double
+    let date: Date
+}
